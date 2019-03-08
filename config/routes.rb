146cc6acdf36dfce4818b2d970543fed8b2235d0
xs1523c <xs1523c@gmail.com>
2019-03-08 08:43:01 +0800

    首頁-img_hover+分頁routes建立

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,25 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root "announcements#index"
+  resources :announcements,only: [:index] do
+    collection do
+      get :travelaround
+    end
+
+    collection do
+      get :foodie
+    end
+
+    collection do
+      get :teamleader
+    end
+
+    collection do
+      get :introduction
+    end
+
+    collection do
+      get :drawing
+    end
+  end
 end
